4e75ae9c3fde78b6f99add6a9fcfa287401800f5
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2021-02-02 14:47:23 -0700

    added the edit and delete card

--- front-end/src/components/UserPage.js
+++ front-end/src/components/UserPage.js
@@ -13,6 +13,20 @@ const UserPage = (props) => {
         <div>
             <h1>Hello, user</h1>
             <Recipes recipes={props.recipes} />
+            <h1>This is my Homepage</h1>
+            <Card className='mb-10'>
+                <Card.Body img src='https://picsum.photos/200/300'>
+                    
+                    <Card.Text>
+                        <h1>This is a new recipie</h1>
+                    </Card.Text>
+                        <button className='editbtn' class='btn' style={{marginLeft: 2 }} >Edit</button>
+                
+                        <button className='deletebtn' class='btn'>Delete</button>
+                </Card.Body>
+            </Card>
+
+
         </div>
     );
 };
