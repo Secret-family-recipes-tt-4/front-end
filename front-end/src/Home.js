1bc255f205e20bc513b8acc956af591955ad4b83
Sebastian <72098066+sebastian-espeset@users.noreply.github.com>
2021-02-04 14:37:22 -0600

    Update Home.js

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -78,21 +78,6 @@ function Home() {
 </Card>
             </div>
             </div>
-=======
-            <h1>This is my Homepage</h1>
-            <Card className='mb-10'>
-                <Card.Body>
-                    
-                    <Card.Text>
-                        <h1>This is a new recipie</h1>
-                    </Card.Text>
-                        <button className='editbtn' class='btn'>Edit</button>
-                
-                        <button className='deletebtn' class='btn'>Delete</button>
-                </Card.Body>
-            </Card>
-        </div>
 
-    );
 }
 export default Home;
