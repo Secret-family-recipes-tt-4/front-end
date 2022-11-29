7a6700046354b30c9500f8defa2d08140963c10b
Anthony Cortese <anthonykcortese@gmail.com>
2022-11-28 23:25:13 -0700

    fix

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -4,88 +4,89 @@ import { connect } from "react-redux";
 import { loadRecipes } from "../store/actions";
 import Recipes from "./Recipes";
 
-const UserPage = (props) => {
-    const [filteredRecipes, setFilteredRecipes] = useState(props.recipes);
-    const [searchWord, setSearchWord] = useState("");
-    useEffect(() => {
-        props.loadRecipes();
-    }, []);
+const UserPage = ({ recipes, loadRecipes }) => {
+  const [filteredRecipes, setFilteredRecipes] = useState(recipes);
+  const [searchWord, setSearchWord] = useState("");
 
-    useEffect(() => {
-        setFilteredRecipes(props.recipes);
-        setSearchWord("");
-    }, [props.recipes]);
+  useEffect(() => {
+    loadRecipes();
+  }, [loadRecipes]);
 
-    useEffect(() => {
-        if (searchWord.length >= 3) {
-            const filtered = props.recipes.filter((recipe) => {
-                if (
-                    recipe.title.toLowerCase().includes(searchWord) ||
-                    recipe.categories.toLowerCase().includes(searchWord)
-                ) {
-                    return recipe;
-                }
-                return null;
-            });
-            setFilteredRecipes(filtered);
-        } else {
-            setFilteredRecipes(props.recipes);
+  useEffect(() => {
+    setFilteredRecipes(recipes);
+    setSearchWord("");
+  }, [recipes]);
+
+  useEffect(() => {
+    if (searchWord.length >= 3) {
+      const filtered = recipes.filter((recipe) => {
+        if (
+          recipe.title.toLowerCase().includes(searchWord) ||
+          recipe.categories.toLowerCase().includes(searchWord)
+        ) {
+          return recipe;
         }
-    }, [props.recipes, searchWord]);
-
-    const handleSearchChange = (e) => {
-        setSearchWord(e.target.value);
-    };
-
-    const handleSearch = (e) => {
-        e.preventDefault();
-        if (searchWord.length >= 3) {
-            const filtered = props.recipes.filter((recipe) => {
-                if (
-                    recipe.title.toLowerCase().includes(searchWord) ||
-                    recipe.categories.toLowerCase().includes(searchWord)
-                ) {
-                    return recipe;
-                }
-                return null;
-            });
-            setFilteredRecipes(filtered);
-        } else {
-            setFilteredRecipes(props.recipes);
-        }
-    };
-
-    if (props.isLoading) {
-        return <div>Loading...</div>;
+        return null;
+      });
+      setFilteredRecipes(filtered);
+    } else {
+      setFilteredRecipes(recipes);
     }
+  }, [recipes, searchWord]);
 
-   
-    return (
-        <div>
-            <h1>Hello, user!</h1>
-            <form className="d-flex">
-                <input
-                    className="form-control me-2"
-                    type="search"
-                    aria-label="Search"
-                    name="search"
-                    value={searchWord}
-                    onChange={handleSearchChange}
-                    placeholder="search by title or category"
-                />
-                <button
-                    className="btn btn-success"
-                    type="submit"
-                    onClick={handleSearch}
-                >
-                    Search
-                </button>
-            </form>
-            <Recipes recipes={filteredRecipes} />
-        </div>
-    );
+  const handleSearchChange = (e) => {
+    setSearchWord(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (searchWord.length >= 3) {
+      const filtered = recipes.filter((recipe) => {
+        if (
+          recipe.title.toLowerCase().includes(searchWord) ||
+          recipe.categories.toLowerCase().includes(searchWord)
+        ) {
+          return recipe;
+        }
+        return null;
+      });
+      setFilteredRecipes(filtered);
+    } else {
+      setFilteredRecipes(recipes);
+    }
+  };
+
+  // eslint-disable-next-line no-undef
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Hello, user!</h1>
+      <form className="d-flex">
+        <input
+          className="form-control me-2"
+          type="search"
+          aria-label="Search"
+          name="search"
+          value={searchWord}
+          onChange={handleSearchChange}
+          placeholder="search by title or category"
+        />
+        <button
+          className="btn btn-success"
+          type="submit"
+          onClick={handleSearch}
+        >
+          Search
+        </button>
+      </form>
+      <Recipes recipes={filteredRecipes} />
+    </div>
+  );
 };
 const mapStateToProps = (state) => {
-    return state;
+  return state;
 };
 export default connect(mapStateToProps, { loadRecipes })(UserPage);
